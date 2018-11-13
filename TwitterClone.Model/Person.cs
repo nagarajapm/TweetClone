51f16ea5112072f07a7c46423c73ae326447fd1f
nagarajapm <43174413+nagarajapm@users.noreply.github.com>
2018-11-13 21:56:42 +0530

    Add files via upload

---
+++ TwitterClone.Model/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone.Model
+{
+    public partial class PERSON
+    {
+        public PERSON()
+        {
+            this.TWEETs = new HashSet<TWEET>();
+            this.Following = new HashSet<PERSON>();
+            this.Followers = new HashSet<PERSON>();
+        }
+
+        public string User_ID { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public System.DateTime Joined { get; set; }
+        public bool Active { get; set; }
+
+        public virtual ICollection<TWEET> TWEETs { get; set; }
+        public virtual ICollection<PERSON> Following { get; set; }
+        public virtual ICollection<PERSON> Followers { get; set; }
+    }
+}
---
+++ TwitterClone.Model/Tweet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone.Model
+{
+    public partial class TWEET
+    {
+        public int Tweet_ID { get; set; }
+        public string User_ID { get; set; }
+        public string Message { get; set; }
+        public System.DateTime Created { get; set; }
+
+
+    }
+}
